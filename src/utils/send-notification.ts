45987b6614b8a708ecc229045df65a359981e266
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2024-03-22 08:56:51 +0630

    update

--- src/utils/send-notification.ts
+++ src/utils/send-notification.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { log } from "starless-logger";
 
-const { FIREBASE_FCM_URL, FIREBASE_FCM_AUTH } = process.env;
+const { FIREBASE_FCM_URL, FIREBASE_FCM_AUTH, IBANKING_URL } = process.env;
 
 export default async function sendNotifiation(body: any) {
   log("Sending notificaion...");
@@ -20,6 +20,17 @@ export default async function sendNotifiation(body: any) {
   );
   if (response.data) {
     log(response.data);
+  }
+  const outsideResponse = await axios.post(
+    `${IBANKING_URL}/api/addoutsidenotification`,
+    {
+      title: body.notification.title,
+      description: body.notification.body,
+      arrivaltime: new Date().toISOString().split("T")[0],
+    }
+  );
+  if (outsideResponse.data) {
+    log(outsideResponse.data);
   }
 
   return response;
